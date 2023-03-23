a7a44ec4e0349f7238987cd69644f99a8ceda11a
John Doe <johndoe@email.com>
2023-03-23 22:48:09 +0000

    Ticket1

--- extract/__init__.py
+++ extract/__init__.py
@@ -6,27 +6,13 @@ filename = "results.csv"
 
 def get_input(filename):
     data = []
-    with open(filename) as f:
-        for line in f.readlines():
-            data.append(line.strip().split(','))
+    with open(filename, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            data.append(row)
 
-# Ticket 2: Remove duplicate entries
-# Add functionality to your input script to ignore or remove any duplicate entries
-# from the input data.
-# Duplicates are based on the User Id field.
+            return data
 
-#Ticket2a: A final array is created with duplicate entries removed.
-#Ticket2b: Where duplicates are found, the first entry is retained.
-
-    with open(filename, 'r') as f_in:
-        lines_set = set()
-
-        for line in f_in:
-            lines_set.add(line)
-
-    with open(filename, 'w') as f_out:
-
-        for line in lines_set:
-            f_out.write(line)
-
-    return data
+file_path = 'results.csv'
+data = get_input(file_path)
+print(data)
