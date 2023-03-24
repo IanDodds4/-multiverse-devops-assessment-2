f9ce4fea430191e2bc25e4dc8769a3e283e1b638
John Doe <johndoe@email.com>
2023-03-24 00:15:54 +0000

    Ticket5

--- extract/__init__.py
+++ extract/__init__.py
@@ -2,30 +2,40 @@
 # Last Edit: 
 
 # Ticket 1: In your input script, create a function that will read data from a CSV file.
+# Date update added: 23/03/2023
 # Objectives:
 #   ● The results.csv data file can be successfully processed into an array.
 #   ● Each line of the file is read into a new array item.
 #   ● The file read method must be in a sub-module.
 
 # Ticket 2: Add functionality to your input script to ignore or remove any duplicate entries from the input data. Duplicates are based on the User Id field.
+# Date update added: 23/03/2023
 # Objectives:
 #   ● A final array is created with duplicate entries removed.
 #   ● Where duplicates are found, the first entry is retained.
 
-# Ticket 3: Description Update your input script to ignore any empty lines found when reading in the input data file.
+# Ticket 3: Update your input script to ignore any empty lines found when reading in the input data file.
+# Date update added: 23/03/2023
 # Objectives 
 #   ● A final array is created with any empty lines omitted.
 
-# Ticket 4: Description Add functionality to your input script to automatically capitalise the first_name and last_name fields found in the input data.
+# Ticket 4: Add functionality to your input script to automatically capitalise the first_name and last_name fields found in the input data.
+# Date update added: 23/03/2023
 # Objectives 
-#    ● All names are capitalised in all data entries.
+#   ● All names are capitalised in all data entries.
+
+# Ticket 5: Update your input script to validate the responses to the third answer field. This answer must have a numeric value between 1 and 10.
+# Any rows with an invalid value are ignored.
+# Objectives 
+#   ● A final array is created with the input data excluding any rows where answer 3 is invalid.
+#   ● No answer 3 values will be outside the range of 1 to 10.
 
 import csv
 
 def get_input(file_path):
 
     data = []
-    user_ids = set() # keep track of seen user ids.
+    user_ids = set() # Ticket 2: Keep track of seen user ids.
 
     with open(file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file) # Ticket 1: Read data from the csv file.
@@ -38,7 +48,14 @@ def get_input(file_path):
             first_name = first_name.strip().capitalize() # Ticket 4: Capitalise first name entry
             last_name = last_name.strip().capitalize() # Ticket 4: Capitalise last name entry
 
-            if user_id not in user_ids: # Ticket 2: For each row in the file, check if the user id is already in the 'user_id' variable, if not then add that id.
+            try:
+                    answer3 = int(answer_3)
+            except ValueError: # Ticket 4: Exclude any invalid answer_3 answers. Ensuring interger values only.
+                    continue
+
+            if user_id not in user_ids and 1 <= answer3 <= 10: 
+                # Ticket 2: For each row in the file, check if the user id is already in the 'user_id' variable, if not then add that id.
+                # Ticket 4: For each row in the file, ensure answer_3 is both greater than or equal to 10 AND less than or equal to 10.
                 data.append([user_id, first_name, last_name, answer_1, answer_2, answer_3])
                 user_ids.add(user_id)
 
