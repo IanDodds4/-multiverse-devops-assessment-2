d4b3a9f52de9be4fe9e1cbf71d5000eadb4c0a22
John Doe <johndoe@email.com>
2023-03-28 18:01:00 +0100

    app.py changes - complete

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import sys
-
 from extract import *
-def main():
-    filename = "results.csv"
-    data = read_csv_file(filename)
 
-if __name__ == "__main__":
-    sys.exit(main())
+filename = "results.csv" # Select input file, add as 'filename' variable.
+data = read_csv_file(filename) # Read the input csv file.
+
+cleanfilename = "clean_results.csv"
+clean_data = write_csv_file(cleanfilename, data) # Write and clean data.
+
+read_clean_csv_file(cleanfilename) # Read clean data into app.
+
+format_clean_csv_file(row) # Output clean dataset into clean results file.
